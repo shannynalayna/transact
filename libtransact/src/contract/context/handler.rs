4e8a3e2c7a98f1aff5dafddc3a00d01033103044
Shannyn Telander <telander@bitwise.io>
2019-12-20 11:24:04 -0600

    WIP ADD TESTS
    Signed-off-by: Shannyn Telander <telander@bitwise.io>

--- libtransact/src/contract/context/handler.rs
+++ libtransact/src/contract/context/handler.rs
@@ -59,3 +59,136 @@ where
         self.apply(transaction, simple_context)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::sync::{Arc, Mutex};
+
+    use crate::contract::address::{
+        double_key_hash::DoubleKeyHashAddresser, key_hash::KeyHashAddresser,
+        triple_key_hash::TripleKeyHashAddresser,
+    };
+
+    use crate::handler::ContextError;
+
+    /// Simple state backed by a HashMap.
+    struct TestState {
+        state: HashMap<String, Vec<u8>>,
+    }
+
+    /// Simple state implementation with basic methods to get, set, and delete state values.
+    impl TestState {
+        pub fn new() -> Self {
+            TestState {
+                state: HashMap::new(),
+            }
+        }
+
+        fn get_entries(
+            &self,
+            addresses: &[String],
+        ) -> Result<Vec<(String, Vec<u8>)>, ContextError> {
+            let mut values = Vec::new();
+            addresses.iter().for_each(|key| {
+                if let Some(value) = self.state.get(key) {
+                    values.push((key.to_string(), value.to_vec()))
+                }
+            });
+            Ok(values)
+        }
+
+        fn set_entries(&mut self, entries: Vec<(String, Vec<u8>)>) -> Result<(), ContextError> {
+            entries.iter().for_each(|(key, value)| {
+                match self.state.insert(key.to_string(), value.to_vec()) {
+                    _ => (),
+                }
+            });
+            Ok(())
+        }
+
+        fn delete_entries(&mut self, addresses: &[String]) -> Result<Vec<String>, ContextError> {
+            let mut deleted = Vec::new();
+            addresses.iter().for_each(|key| {
+                if let Some(_) = self.state.remove(key) {
+                    deleted.push(key.to_string());
+                }
+            });
+            Ok(deleted)
+        }
+
+        fn add_receipt_data(&self, _data: Vec<u8>) -> Result<(), ContextError> {
+            Ok(())
+        }
+
+        fn add_event(
+            &self,
+            _event_type: String,
+            _attributes: Vec<(String, String)>,
+            _data: Vec<u8>,
+        ) -> Result<(), ContextError> {
+            Ok(())
+        }
+    }
+
+    /// TestContext using the simple TestState backend to be used as the internal context to
+    /// construct a KeyValueTransactionContext for the tests.
+    struct TestContext {
+        internal_state: Arc<Mutex<TestState>>,
+    }
+
+    impl TestContext {
+        pub fn new() -> Self {
+            TestContext {
+                internal_state: Arc::new(Mutex::new(TestState::new())),
+            }
+        }
+    }
+
+    /// TransactionContext trait implementation for the TestContext.
+    impl TransactionContext for TestContext {
+        fn get_state_entries(
+            &self,
+            addresses: &[String],
+        ) -> Result<Vec<(String, Vec<u8>)>, ContextError> {
+            self.internal_state
+                .lock()
+                .expect("Test lock was poisoned in get method")
+                .get_entries(addresses)
+        }
+
+        fn set_state_entries(&self, entries: Vec<(String, Vec<u8>)>) -> Result<(), ContextError> {
+            self.internal_state
+                .lock()
+                .expect("Test lock was poisoned in set method")
+                .set_entries(entries)
+        }
+
+        fn delete_state_entries(&self, addresses: &[String]) -> Result<Vec<String>, ContextError> {
+            self.internal_state
+                .lock()
+                .expect("Test lock was poisoned in delete method")
+                .delete_entries(addresses)
+        }
+
+        fn add_receipt_data(&self, data: Vec<u8>) -> Result<(), ContextError> {
+            self.internal_state
+                .lock()
+                .expect("Test lock was poisoned in add_receipt_data method")
+                .add_receipt_data(data)
+        }
+
+        fn add_event(
+            &self,
+            event_type: String,
+            attributes: Vec<(String, String)>,
+            data: Vec<u8>,
+        ) -> Result<(), ContextError> {
+            self.internal_state
+                .lock()
+                .expect("Test lock was poisoned in add_event method")
+                .add_event(event_type, attributes, data)
+        }
+    }
+}
